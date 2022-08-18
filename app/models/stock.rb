894675446cd574236a88b278a9fcbaff290b4cc3
steefg3000 <steef.gijsbers@gmail.com>
2022-08-18 22:04:14 +0200

    Add user stock tracking functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,9 +8,13 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key], 
                                   endpoint: 'https://sandbox.iexapis.com/v1')
     begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      new(ticker: client.company(ticker_symbol).symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
